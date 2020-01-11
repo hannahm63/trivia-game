132e4219dba1df46a356ea4fda30d6a58b3e7e38
hannahm63 <52358146+hannahm63@users.noreply.github.com>
2020-01-11 15:58:56 -0700

    added timer clock to bottom of question area and added answer choice values as a custom data value for each radio button choice to compare to correct answer when selected

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,66 +17,109 @@ $(document).ready(function () {
         choices: ["reptiles", "invertebrates", "rodents", "canines"]
     }];
 
+    // div where start button and answer choices will be displayed
+    let inputRow = $("#input");
+
     console.log(qAndA[0].q);
 
-    // add start btn to row 3 with message 
-    let inputRow = $("#input");
-    let btnStart = $("<button>")
-        .addClass("btn btn-outline-success btn-lg btn-block")
-        .text("Start");
+    function startScreen() {
+        // add start btn to row 3 with message 
+        let btnStart = $("<button>")
+            .addClass("btn btn-outline-success btn-lg btn-block")
+            .text("Start");
 
-    inputRow.append(btnStart);
+        inputRow.append(btnStart);
 
-    // when start btn is clicked
-    btnStart.on("click", function () {
-        console.log("clicked!");
+        // when start btn is clicked
+        btnStart.on("click", function () {
+            console.log("clicked!");
 
-        // start btn and message are removed
-        btnStart.remove();
+            // start btn and message are removed
+            btnStart.remove();
 
-        // questions are displayed
-        displayQuestions();
+            // questions are displayed
+            displayQuestions();
 
-    });
+        });
+    };
 
-    function displayQuestions() { 
-        
+    function displayQuestions() {
+
         // replace <p> text with question to row 2
+        $("#message").text(qAndA[1].q);
 
-        
-        // append answer choices to row 3
-        // append timer to row 4  
-        // start 15 second count down
+
+        // create answer div & fill answer div with each answer choice
+        for (let i = 0; i < qAndA[1].choices.length; i++) {
+            let answers = $("<label>")
+                .text(qAndA[1].choices[i]);
+            answers.prepend($(`<input type="radio" data-answer=${qAndA[1].choices[i]}>`));
+
+            // append answer choices to row 3
+            inputRow.append(answers);
+        };
+
+        timer();
 
     };
 
     // when any answer (class) is clicked 
     // if correct answer, display correct page
     // if incorrect answer, display incorrect page
+
     // if time runs out, display time's up page
 
+    // define timer for questions
+    function timer() {
+        var timeLeft = 10;
+        var intervalId = setInterval(decrement, 1000);
+        $("#row4").html(`<p>${timeLeft} seconds</p>`);
+
+        function decrement() {
+            timeLeft--;
+            $("#row4").html(`<p>${timeLeft} seconds</p>`);
+            if (timeLeft === 0) {
+              stop();
+              alert("Time Up!");
+            }
+          }
+          function stop() {
+            clearInterval(intervalId);
+          }
+
+        // append timer to row 4  
+        // start 15 second count down
+    };
+
     // correct answer fxn
-    // row 2 text shows correct
-    // row 3 displays correct answer
-    // row 4 displays image
-    // 5 second timer
+    function correctAnswer() {
+        // row 2 text shows correct
+        // row 3 displays correct answer
+        // row 4 displays image
+        // 5 second timer
+    };
 
     // incorrect answer fxn
-    // row 2 text shows incorrect
-    // row 3 displays correct answer
-    // row 4 displays image
-    // 5 second timer
+    function incorrectAnswer() {
+        // row 2 text shows incorrect
+        // row 3 displays correct answer
+        // row 4 displays image
+        // 5 second timer
+    };
 
     // time's up fxn
-    // row 2 text shows time is up
-    // row 3 displays correct answer
-    // row 4 displays image
-    // 5 second timer
+    function outOfTime() {
+        // row 2 text shows time is up
+        // row 3 displays correct answer
+        // row 4 displays image
+        // 5 second timer
+    };
 
     // once all questions have been answered
     // row 2 displays game over message
     // row 3 displays start button
     // row 4 displays correct and incorrect answer counts
 
+    startScreen();
 
 });
