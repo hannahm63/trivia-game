e2c64b7fe9f2bf233e3fada997fbaa9c458ff492
hannahm63 <52358146+hannahm63@users.noreply.github.com>
2020-01-12 00:12:29 -0700

    added counter for correct and incorrect questions, wrote out gameOver and tryAgain functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,6 +20,8 @@ $(document).ready(function () {
     // div where start button and answer choices will be displayed
     let inputRow = $("#input");
     let questionCount = 0;
+    let correctCount = 0;
+    let incorrectCount = 0;
     var displayMessage;
     var intervalId;
 
@@ -96,6 +98,7 @@ $(document).ready(function () {
 
     // correct answer fxn
     function correctAnswer() {
+        correctCount++;
         console.log(`correct answer method called`);
         // remove timer from display
         $("#row4").empty();
@@ -105,25 +108,27 @@ $(document).ready(function () {
         $("#input").text(`The correct answer is ${qAndA[questionCount].a}.`)
         // row 4 displays image
         // 5 second timer
-        displayMessage = setTimeout(nextQuestion, 3000);
+        displayMessage = setTimeout(nextQuestion, 2000);
     };
 
     // incorrect answer fxn
     function incorrectAnswer() {
+        incorrectCount++;
         console.log(`incorrect answer method called`);
         // remove timer from display
         $("#row4").empty();
         // row 2 text shows correct
-        $("#message").text(`Sorry! That was not correct...`)
+        $("#message").text(`Sorry! That was not correct...`);
         // row 3 displays correct answer
-        $("#input").text(`The correct answer is ${qAndA[questionCount].a}.`)
+        $("#input").text(`The correct answer is ${qAndA[questionCount].a}.`);
         // row 4 displays image
         // 5 second timer
-        displayMessage = setTimeout(nextQuestion, 3000);
+        displayMessage = setTimeout(nextQuestion, 2000);
     };
 
     // time's up fxn
     function outOfTime() {
+        incorrectCount++;
         console.log(`out of time method called`);
         // remove timer from display
         $("#row4").empty();
@@ -131,14 +136,14 @@ $(document).ready(function () {
         $("#message").text(`You ran out of time for this question!`)
         // row 3 displays correct answer
         $("#input").text(`The correct answer is ${qAndA[questionCount].a}.`)
-        displayMessage = setTimeout(nextQuestion, 3000);
+        displayMessage = setTimeout(nextQuestion, 2000);
     };
 
     function nextQuestion() {
         console.log(`nextQuestion method called`);
         clearTimeout(displayMessage);
         questionCount++;
-        if (questionCount < qAndA.length - 1) {
+        if (questionCount < qAndA.length) {
             displayQuestions(questionCount);
         } else {
             gameOver();
@@ -149,16 +154,37 @@ $(document).ready(function () {
     function gameOver() {
         console.log(`gameover method called`);
         // row 2 displays game over message
-        // row 3 displays try again button - onclick calls restart()
-        // row 4 displays correct and incorrect answer counts
+        $("#message").text(`You have completed the quiz! Here are your results...`);
+        // row 3 displays correct and incorrect answer counts
+        inputRow.empty();
+        inputRow.html(`<p>You answered ${correctCount} questions correctly and ${incorrectCount} incorrectly!`);
+        // row 4 displays try again button - onclick calls tryAgain()
+        let btnTryAgain = $("<button>")
+            .addClass("btn btn-outline-success btn-lg btn-block")
+            .attr('id', 'tryAgain')
+            .text("Try Again");
+
+        inputRow.append(btnTryAgain);
+
+        $("body").on("click", "#tryAgain", function () {
+            tryAgain();
+        });
+
     };
 
     // restart fxn
-    function restart() {
+    function tryAgain() {
         console.log(`restart method called`);
-        // resets question count to 0
+        // resets counts to 0
+        questionCount = 0;
+        correctCount = 0;
+        incorrectCount = 0;
         // removes each element
+        inputRow.empty();
+        $("#message").empty();
+        $("#row4").empty();
         // calls displayQuestions()
+        displayQuestions(questionCount);
     };
 
 
@@ -172,8 +198,10 @@ $(document).ready(function () {
 
         // if correct answer, display correct page otherwise display incorrect page
         if (selectedRadio === qAndA[questionCount].a) {
+            console.log(correctCount);
             correctAnswer();
         } else {
+            console.log(incorrectCount);
             incorrectAnswer();
         }
     });
